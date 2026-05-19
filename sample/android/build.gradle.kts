2c8162221fad387fbf4116eb82dc48bb43c485a0
Moritz Lindner <linde9898@proton.me>
2026-05-19 15:17:00 +0200

    update android dependency

--- sample/android/build.gradle.kts
+++ sample/android/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     id("com.android.application") version "9.2.1"
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21"
-    id("org.jetbrains.compose") version "1.10.3"
+    id("org.jetbrains.compose") version "1.11.0"
 }
 
 android {
     namespace = "io.github.linde9821.treelayout.sample.android"
-    compileSdk = 36
+    compileSdk = 37
     defaultConfig {
         applicationId = "io.github.linde9821.treelayout.sample.android"
         minSdk = 24
@@ -21,7 +21,7 @@ android {
 
 dependencies {
     implementation("io.github.linde9821:treelayout-kmp:0.2.0")
-    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.activity:activity-compose:1.13.0")
     implementation(compose.runtime)
     implementation(compose.foundation)
     implementation(compose.material)
